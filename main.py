067b231d316e523e2eca8327256c528607a8a396
kevin <kchristian03@student.ciputra.ac.id>
2023-12-10 22:33:10 +0700

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+#make main function to run the program
+def count_vowels(user_input):
+    pass
+
+
+def main():
+    #make a variable to store the user input
+    user_input = input("Enter a string: ")
+    #call the function to count the vowels
+    count_vowels(user_input)
+    
+    
+#make __main__ function to run the main function
+if __name__ == "__main__":
+    main()
