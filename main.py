50c2ff5742cab757b5ff0f7adcb9c4b2131061a3
kevin <kchristian03@student.ciputra.ac.id>
2023-12-10 23:33:16 +0700

    coba 1

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 #make main function to run the program
-def count_vowels(user_input):
-    pass
-
-
 def main():
-    #make a variable to store the user input
-    user_input = input("Enter a string: ")
-    #call the function to count the vowels
-    count_vowels(user_input)
+    print("Hello ALP")
     
     
 #make __main__ function to run the main function
