b0bc4e77029ae2ab2dbe2a4d5c81033a1ce8670c
JasonEmanuel <91118585+JasonEmanuel@users.noreply.github.com>
2024-01-06 17:48:00 +0700

    pkl file

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,15 @@
+import joblib
 from flask import Flask, request, jsonify
-import your_stroke_prediction_model  # Import your model
+
+# import ada_boost_model  # Import your model
+loaded_model = joblib.load('model/ada_boost_model.pkl')
 
 app = Flask(__name__)
 
 @app.route('/predict_stroke', methods=['POST'])
 def predict_stroke():
     data = request.get_json()  # Get the data from the PHP request
-    prediction = your_stroke_prediction_model.predict(data)  # Use your model to predict
+    prediction = loaded_model.predict(data)  # Use your model to predict
     return jsonify({'prediction': prediction})  # Return the prediction as JSON
 
 if __name__ == '__main__':
